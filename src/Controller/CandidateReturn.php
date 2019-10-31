a53ca2457e6343d42a3cf9ffad2360a022b48460
Nyholm <tobias.nyholm@gmail.com>
2019-10-31 19:47:48 +0100

    Added candidate return

--- src/Controller/CandidateReturn.php
+++ src/Controller/CandidateReturn.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\Database;
+
 /**
  * This controller is used when a candidate returns form a test.
  *
@@ -12,6 +14,11 @@ class CandidateReturn
 {
     public function run($url)
     {
-        echo 'step 3';
+        parse_str(parse_url($url, PHP_URL_QUERY), $query);
+
+        Database::storeCandidate($query['candidate-id']);
+
+        echo "Welcome back from doing the test. We saved the candidate id";
+        echo '<a href="/dashboard">Back to Dashboard</a>';
     }
 }
